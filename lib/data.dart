b7f390540be228e950afd240f7fb4ea8754a43bd
Ayesha <ayesha@nextgeni.dev>
2022-05-19 17:50:49 +0500

    Initial commit

---
+++ lib/data.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/models/category.dart';
+
+const DUMMY_CATEGORIES = [
+  Category(id: 'c1', title: 'Italian', color: Colors.purple),
+  Category(id: 'c2', title: 'Quick & Easy', color: Colors.red),
+  Category(id: 'c3', title: 'Test', color: Colors.pinkAccent)
+];
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import './category_meals_screen.dart';
+
+import 'categories_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Deli Meal',
+      theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
+              .copyWith(secondary: Colors.amber),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              titleLarge: const TextStyle(
+                  fontFamily: 'Raleway',
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
+              // bodyText1:
+              //     const TextStyle(color: Color.fromRGBO(20, 51, 51, 1))
+          )),
+      home: CategoriesScreen(title: 'DeliMeals'),
+      routes: {
+        '/categories':(context)=>CategoryMealsScreen(),
+      },
+    );
+  }
+}
